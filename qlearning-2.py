e8ab7f2eca079890ccee047a79c5fc7f5f1480ad
Serhii Kostel <dodge.ksv@gmail.com>
2019-09-21 16:40:37 +0300

    Added training visualization

--- qlearning-2.py
+++ qlearning-2.py
@@ -2,6 +2,7 @@
 
 import gym
 import numpy as np
+import cv2
 
 env = gym.make('MountainCar-v0')
 
@@ -10,6 +11,7 @@ DISCOUNT = 0.95
 EPISODES = 5000
 
 SHOW_EVERY = 500
+SHOW_TRAINING = True
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)  # covered all the space with 20 x 20 = 400 discrete points
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
@@ -31,6 +33,21 @@ def get_discrete_state(state):
     """Convert continuous state to discrete"""
     dis_state = (state - env.observation_space.low) / discrete_os_win_size
     return tuple(dis_state.astype(np.int))
+
+
+def show(title, img, flip_horizontal=True, scale=1):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    if flip_horizontal:
+        img = cv2.flip(img, 0)
+
+    if scale > 1:
+        img = cv2.resize(img,
+            dsize=(img.shape[1] * scale, img.shape[0] * scale),
+            interpolation=cv2.INTER_NEAREST)
+
+    cv2.imshow(title, img)
+    cv2.waitKey(1)
 
 
 for episode in range(EPISODES):
@@ -72,5 +89,17 @@ for episode in range(EPISODES):
 
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
+
+    if SHOW_TRAINING:
+        action_img = np.argmax(q_table, axis=2)
+        q_table_img = ((q_table / np.abs(q_table.min()) + 1) * [255, 255, 255]).astype(np.uint8)
+
+        q_action_img = np.hstack((
+            np.stack(
+                [((action_img == i) * 255).astype(np.uint8) for i in range(3)],
+                axis=-1),
+            q_table_img))
+
+        show('Action and Q table', q_action_img, scale=10)
 
 env.close()
